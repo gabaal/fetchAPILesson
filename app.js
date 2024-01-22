77dc688bb00983ef38bf3d786884b19e82f5718a
gabaal <geraldbaalham@live.co.uk>
2024-01-22 11:52:23 +0000

    Lesson complete

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 console.log("Working");
+
+// async await function
+
+// async function getMyStuffFromOverThere() {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//   console.log("HTTP Response:", response);
+//   const json = await response.json();
+//   console.log("JSON Data:", json);
+// }
+//
+
+//then method
+
+function getMyStuffFromOverThere() {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then(function (response) {
+      console.log("HTTP Response:", response);
+      return response.json();
+    })
+    .then(function (json) {
+      console.log("JSON Data:", json);
+    });
+}
+
+//getMyStuffFromOverThere();
+
+//Parsing the responce
+
+async function getGitHub() {
+  const response = await fetch("https://api.github.com/repos/gabaal/ToDoList");
+  console.log("HTTP Response:", response);
+  const json = await response.json();
+  console.log("JSON Data:", json);
+}
+getGitHub();
